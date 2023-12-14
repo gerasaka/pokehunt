35b3740124eb9f67435eb5ffebc71bc417ae7410
Gerry Julio <gerry.julio@doku.com>
2023-12-15 07:53:25 +0800

    chore: setup webpack

---
+++ webpack.common.js
@@ -0,0 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [{ test: /\.css$/, use: ["style-loader","css-loader"] }],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
