725a58858928ff09bbb13f6c0d1fd575bb169ab3
gerasaka <gerryjulioo@gmail.com>
2023-12-22 15:59:34 +0800

    feat: map pokemon data to main page

--- src/script/main.js
+++ src/script/main.js
@@ -2,8 +2,33 @@ import { PokemonService } from "./pokemon.service";
 
 const pokemonService = new PokemonService();
 
-const main = () => {
-  pokemonService.generatePokemonList();
+const main = async () => {
+  await pokemonService.generatePokemonList();
+  const listContainer = document.getElementById("list-container");
+
+  const listItem = (pokemon) => {
+    const container = document.createElement("a");
+    container.setAttribute(
+      "class",
+      "hover:bg-ph-dark-blue border-ph-dark-blue text-ph-blue hover:text-ph-yellow mt-8 rounded-lg border transition hover:shadow-xl p-3",
+    );
+
+    container.innerHTML = `
+      <img
+        src=${pokemon.details.imageUrl}
+        alt="image of ${pokemon.name}"
+        class="w-full"
+        style="margin-top: -35%"
+      />
+      <h2 class="text-center text-xl font-bold md:text-2xl lg:text-3xl">${pokemon.name}</h2>
+    `;
+
+    return container;
+  };
+
+  pokemonService.pokemonList.forEach((pokemon) => {
+    listContainer.appendChild(listItem(pokemon));
+  });
 };
 
 export default main;
