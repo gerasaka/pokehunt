3f340557aa21a6ecdaa772db89b4e530c2d8ebab
gerasaka <gerryjulioo@gmail.com>
2023-12-31 06:21:15 +0800

    feat: change ui themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,26 +18,33 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        lemonade: {
+        lightPoke: {
           primary: "#fecb01",
           secondary: "#355fab",
-          ".btn-nav": {
-            color: "#355fab",
-            border: "1px solid #355fab",
-            display: "flex",
-            "justify-content": "center",
-            "align-items": "center",
-            "border-radius": "9999px",
-          },
-          ".btn-nav:hover": {
-            color: "#fecb01",
-            "background-color": "#355fab",
-          },
-          ".btn-nav:disabled": {
-            "background-color": "whitesmoke",
-            color: "slategray",
-            border: "none",
-          },
+          accent: "#ef4444",
+          neutral: "#047857",
+          "base-100": "#f7fdee",
+          info: "#d97706",
+          success: "#4ade80",
+          warning: "#b91c1c",
+          error: "#7e22ce",
+          // ".btn-nav": {
+          //   color: "#355fab",
+          //   border: "1px solid #355fab",
+          //   display: "flex",
+          //   "justify-content": "center",
+          //   "align-items": "center",
+          //   "border-radius": "9999px",
+          // },
+          // ".btn-nav:hover": {
+          //   color: "#fecb01",
+          //   "background-color": "#355fab",
+          // },
+          // ".btn-nav:disabled": {
+          //   "background-color": "whitesmoke",
+          //   color: "slategray",
+          //   border: "none",
+          // },
         },
       },
     ],
