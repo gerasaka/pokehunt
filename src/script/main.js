586ed717d74adbdd2a84069e210d6b4420acc12f
gerasaka <gerryjulioo@gmail.com>
2023-12-31 15:57:55 +0800

    feat: add modal when search result error and implement first page and last page feature

--- src/script/main.js
+++ src/script/main.js
@@ -5,11 +5,17 @@ const pokemonService = new PokemonService();
 
 const listContainer = document.getElementById("list-container");
 
-const prevbtn = document.getElementById("prev-btn");
-const nextbtn = document.getElementById("next-btn");
+// Pagination variables
 const currIndicator = document.getElementById("curr-page");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+const firstBtn = document.getElementById("first-btn");
+const lastBtn = document.getElementById("last-btn");
 
+// Search variables
 const searchInput = document.getElementById("search-input");
+const notFoundModal = document.getElementById("not-found-modal");
+const errorModal = document.getElementById("error-modal");
 document.getElementById("search-btn").addEventListener("click", searchPokemon);
 
 const loadPokemonList = () => {
@@ -17,7 +23,7 @@ const loadPokemonList = () => {
     const container = document.createElement("a");
     container.setAttribute(
       "class",
-      "hover:bg-ph-dark-blue border-ph-dark-blue text-ph-blue hover:text-ph-yellow mt-8 rounded-lg border p-3 transition hover:shadow-xl",
+      "hover:bg-ph-dark-blue border-ph-dark-blue text-secondary hover:text-primary mt-8 rounded-lg border p-3 transition hover:shadow-xl",
     );
     container.setAttribute("href", `http://localhost:8080/details.html?id=${index}`);
     container.addEventListener("click", () => (pokemonService.pokemonDetails = pokemon));
@@ -42,8 +48,22 @@ const loadPokemonList = () => {
     listContainer.appendChild(listItem(pokemon, i));
   });
 
-  if (pokemonService.previous) prevbtn.removeAttribute("disabled");
-  if (pokemonService.next) nextbtn.removeAttribute("disabled");
+  setNavigation();
+};
+
+const setNavigation = () => {
+  if (pokemonService.currentPage === 1) firstBtn.disabled = true;
+  else firstBtn.disabled = false;
+
+  if (pokemonService.currentPage === pokemonService.lastPageNumber) lastBtn.disabled = true;
+  else lastBtn.disabled = false;
+
+  if (pokemonService.previous) prevBtn.disabled = false;
+  else prevBtn.disabled = true;
+
+  if (pokemonService.next) nextBtn.disabled = false;
+  else nextBtn.disabled = true;
+
   currIndicator.innerHTML = `Page ${pokemonService.currentPage}`;
 };
 
@@ -53,27 +73,16 @@ const renderLoadingState = () => {
     .join("");
 };
 
-const prev = () => {
+const changePage = (endpoint, setPageNumber, revertPageNumber) => {
   renderLoadingState();
-  pokemonService.currentPage -= 1;
-  pokemonService
-    .generatePokemonList(pokemonService.previous)
-    .then(() => loadPokemonList())
-    .catch((e) => {
-      pokemonService.currentPage += 1;
-      console.error("error when get previous data", e);
-    });
-};
+  setPageNumber();
 
-const next = () => {
-  renderLoadingState();
-  pokemonService.currentPage += 1;
   pokemonService
-    .generatePokemonList(pokemonService.next)
+    .generatePokemonList(endpoint)
     .then(() => loadPokemonList())
-    .catch((e) => {
-      pokemonService.currentPage -= 1;
-      console.error("error when get next data", e);
+    .catch(() => {
+      revertPageNumber();
+      errorModal.showModal();
     });
 };
 
@@ -85,14 +94,15 @@ async function searchPokemon() {
     return;
   }
 
-  try {
-    const data = await pokemonService.searchPokemon(query);
+  const res = await pokemonService.searchPokemon(query);
+
+  if (res.status === 200) {
+    const data = pokemonService.cleanPokemonDetails(await res.json());
     pokemonService.pokemonDetails = data;
     window.location.href = `http://localhost:8080/details.html?id=${data.id}`;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    alert("Pokémon not found. Please try another search term.");
-  }
+  } else if (res.status === 404) {
+    notFoundModal.showModal();
+  } else errorModal.showModal();
 }
 
 const main = async () => {
@@ -104,8 +114,38 @@ const main = async () => {
 
   loadPokemonList();
 
-  prevbtn.addEventListener("click", prev);
-  nextbtn.addEventListener("click", next);
+  const sessionPageNumber = () => {
+    return JSON.parse(sessionStorage.getItem("pokeSession")).activePage;
+  };
+
+  prevBtn.addEventListener("click", () =>
+    changePage(
+      pokemonService.previous,
+      () => (pokemonService.currentPage -= 1),
+      () => (pokemonService.currentPage += 1),
+    ),
+  );
+  nextBtn.addEventListener("click", () =>
+    changePage(
+      pokemonService.next,
+      () => (pokemonService.currentPage += 1),
+      () => (pokemonService.currentPage -= 1),
+    ),
+  );
+  firstBtn.addEventListener("click", () =>
+    changePage(
+      `https://pokeapi.co/api/v2/pokemon`,
+      () => (pokemonService.currentPage = 1),
+      () => (pokemonService.currentPage = sessionPageNumber()),
+    ),
+  );
+  lastBtn.addEventListener("click", () =>
+    changePage(
+      pokemonService.last,
+      () => (pokemonService.currentPage = pokemonService.lastPageNumber),
+      () => (pokemonService.currentPage = sessionPageNumber()),
+    ),
+  );
 };
 
 document.addEventListener("DOMContentLoaded", main);
--- src/script/pokemon.service.js
+++ src/script/pokemon.service.js
@@ -2,6 +2,8 @@ export class PokemonService {
   pokemonList = [];
   next = null;
   previous = null;
+  last = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
+  lastPageNumber = 0;
   _activePage = {
     number: 1,
     listUrl: "",
@@ -54,8 +56,12 @@ export class PokemonService {
     return fetch(listUrl)
       .then((response) => response.json())
       .then((data) => {
+        const lastPage = Math.ceil(data.count / 20);
+
         this.next = data.next;
         this.previous = data.previous;
+        this.last = `https://pokeapi.co/api/v2/pokemon?offset=${(lastPage - 1) * 20}&limit=20`;
+        this.lastPageNumber = lastPage;
         return data.results;
       })
       .catch((e) => console.error("error when getting pokemon list", e));
@@ -108,7 +114,6 @@ export class PokemonService {
   }
 
   async searchPokemon(query) {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
-    return this.cleanPokemonDetails(await res.json());
+    return await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
   }
 }
