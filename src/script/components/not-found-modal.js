ee22b2d6ee43edd1e4c998814d73f8aca4cfd2b8
gerasaka <gerryjulioo@gmail.com>
2024-01-03 07:35:13 +0800

    feat: add fallback image

--- src/script/components/not-found-modal.js
+++ src/script/components/not-found-modal.js
@@ -1,3 +1,5 @@
+import "../../assets/pokeball-logo.png";
+
 export class NotFoundModal extends HTMLElement {
   connectedCallback() {
     this.render();
--- src/script/components/poke-card.js
+++ src/script/components/poke-card.js
@@ -1,3 +1,5 @@
+import "../../assets/no-image.png";
+
 export class PokeCard extends HTMLElement {
   connectedCallback() {
     this.render();
@@ -13,30 +15,26 @@ export class PokeCard extends HTMLElement {
   }
 
   render() {
-    this.innerHTML = `
-      <a class="">
-        <img class="w-full" style="margin-top: -35%">
-        <h2 class="text-center text-xl font-bold md:text-2xl"></h2>
-      </a>
-    `;
-
-    const imageUrl = this.getAttribute("image");
-    const name = this.getAttribute("name");
-    const href = this.getAttribute("href");
-
-    const anchor = this.querySelector("a");
-    const image = this.querySelector("img");
-    const heading = this.querySelector("h2");
-
     this.setAttribute(
       "class",
       "hover:bg-ph-dark-blue border-ph-dark-blue text-secondary hover:text-primary mt-8 rounded-lg border p-3 transition hover:shadow-xl",
     );
 
-    anchor.href = href;
+    this.innerHTML = `
+      <a class="">
+        <img class="w-full" style="margin-top: -35%" onerror="this.onerror=null;this.src='./assets/no-image.png';this.setAttribute('class', 'p-4')">
+        <h2 class="text-center text-xl font-bold md:text-2xl"></h2>
+      </a>
+    `;
+
+    this.querySelector("a").href = this.getAttribute("href");
+    this.querySelector("h2").textContent = this.getAttribute("name");
+
+    const imageUrl = this.getAttribute("image");
+    const image = this.querySelector("img");
+
     image.src = imageUrl;
     image.alt = `image of ${name}`;
-    heading.textContent = name;
   }
 }
 
